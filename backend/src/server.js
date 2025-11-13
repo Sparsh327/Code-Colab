4ffd553c8b3d3ab7ea49ab0a2157ca87d501b66d
sparshjaiswal <sparsh.jas07@gmail.com>
2025-11-13 20:35:06 +0530

    updated webhook

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ import { clerkMiddleware } from "@clerk/express";
 
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
-// import { inngest, functions } from "./lib/inngest.js";
+import { inngest, functions } from "./lib/inngest.js";
 
 // import chatRoutes from "./routes/chatRoutes.js";
 // import sessionRoutes from "./routes/sessionRoute.js";
@@ -18,10 +18,10 @@ const __dirname = path.resolve();
 // middleware
 app.use(express.json());
 // credentials:true meaning?? => server allows a browser to include cookies on request
-// app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
-// app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
-// app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/inngest", serve({ client: inngest, functions }));
 // app.use("/api/chat", chatRoutes);
 // app.use("/api/sessions", sessionRoutes);
 
