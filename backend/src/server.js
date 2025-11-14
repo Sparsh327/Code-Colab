76262c31bf008951735a89ebfec9e27d0c58ebef
sparshjaiswal <sparsh.jas07@gmail.com>
2025-11-14 18:49:38 +0530

    created apis for session and chat room

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,9 +8,9 @@ import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 import { inngest, functions } from "./lib/inngest.js";
 
-// import chatRoutes from "./routes/chatRoutes.js";
-// import sessionRoutes from "./routes/sessionRoute.js";
-
+import chatRoutes from "./routes/chatRoutes.js";
+import sessionRoutes from "./routes/sessionRoute.js";
+import { protectRoute } from "./middleware/protectRoute.js";
 const app = express();
 
 const __dirname = path.resolve();
@@ -22,11 +22,15 @@ app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
-// app.use("/api/chat", chatRoutes);
-// app.use("/api/sessions", sessionRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/sessions", sessionRoutes);
 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
+});
+
+app.get("/getBooks", protectRoute, (req, res) => {
+  res.send("Welcome to the Code Colab Backend API");
 });
 
 // make our app ready for deployment
@@ -42,7 +46,7 @@ const startServer = async () => {
   try {
     await connectDB();
     app.listen(ENV.PORT, () =>
-      console.log("Server is running on port:", ENV.PORT)
+      console.log("Server is running on port: http://localhost:3000", ENV.PORT)
     );
   } catch (error) {
     console.error("💥 Error starting the server", error);
